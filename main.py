9c2d9322d5751d95360652a25f544834e707b307
Prem Kumar Rasakonda <117320898+prem8667@users.noreply.github.com>
2024-04-01 15:12:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchainhelper import get_few_shot_db_chain
 
-st.title("AtliQ T Shirts: Database Q&A 👕")
+st.title("chatbot")
 
 question = st.text_input("Question: ")
 
